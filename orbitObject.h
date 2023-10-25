fa0041562c102a51e447af3df25b95461275eede
BuildTools <unconfigured@null.spigotmc.org>
2023-10-25 15:19:03 -0600

    GPS satellite basic implementation

--- orbitObject.h
+++ orbitObject.h
@@ -10,16 +10,31 @@
 #pragma once
 
 #include "position.h"
+#include "velocity.h"
+#include "acceleration.h"
+#include "uiDraw.h"     // for RANDOM and DRAW*
 
 class OrbitObject
 {
-private:
+protected:
    Position position;
-   //Velocity velocity;
+   Velocity velocity;
+   Acceleration acceleration;
+   double angle;
 
 public:
+   OrbitObject() : position(), velocity(), acceleration(), angle(0.0) {}
+
    Position& getPosition() { return position; }
+   Velocity& getVelocity() { return velocity; }
+   Acceleration& getAcceleration() { return acceleration; }
+   double& getAngle() { return angle; }
 
    void setPosition(const Position& position) { this->position = position; }
+   void setVelocity(const Velocity& velocity) { this->velocity = velocity; }
+   void setAcceleration(const Acceleration& acceleration) { this->acceleration = acceleration; }
+   void setAngle(const double angle) { this->angle = angle; }
+
+   virtual void draw(ogstream gout) const = 0;
 };
 
