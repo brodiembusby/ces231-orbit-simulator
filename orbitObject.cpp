4f46efefe61773a1b21fb966e5b46222b2148120
Brodie M Busby <93949221+brodiembusby@users.noreply.github.com>
2023-12-02 15:38:37 -0700

    Sputnik fragments

--- orbitObject.cpp
+++ orbitObject.cpp
@@ -10,6 +10,7 @@
 #include "orbitObject.h"
 #include "acceleration.h"
 #include "velocity.h"
+#include "fragment.h"
 #pragma once
  /************************************************************************
  * UPDATE
@@ -44,6 +45,32 @@ const bool OrbitObject::checkCollision(const OrbitObject& other) const
 }
 
 void OrbitObject::breakApart(std::vector<OrbitObject*>& orbitObjects) const {
+   Velocity vel(velocity);
+   vel.addMetersX(1000);
 
+   const int fragmentCount = 5; // Number of fragments
+   Fragment* fragments[fragmentCount];
+   const int spacing = 100; // Spacing in degrees (for a circular pattern)
+
+   for (int i = 0; i < fragmentCount; i++) {
+      fragments[i] = new Fragment();
+      Position pos(position);
+
+      // Calculate the position for each fragment in a circular pattern
+      float angle = (spacing * i) * (M_PI / 180.0); // Convert degrees to radians
+      float xOffset = cos(angle) * 10; // 10 is the radius of the circle
+      float yOffset = sin(angle) * 10;
+
+      pos.addPixelsX(xOffset);
+      pos.addPixelsY(yOffset);
+
+      // Generate a random rotation for each fragment
+      float randomRotation = static_cast<float>(rand() % 360); // Random angle between 0 and 359 degrees
+
+      fragments[i]->setRotation(randomRotation);
+      fragments[i]->setPosition(pos);
+      fragments[i]->setVelocity(vel);
+      orbitObjects.push_back(fragments[i]);
+   }
    
 }
--- sputnik.cpp
+++ sputnik.cpp
@@ -11,24 +11,35 @@
 #pragma once
 #include "sputnik.h"
 #include "fragment.h"
-//Sputnik should only have fragments but i kept getting a compiler error
-//without this idk
-void Sputnik::breakApart(std::vector<OrbitObject*>& orbitObjects) const {
+#include <cmath> // Include cmath for trigonometric functions
+#include <cstdlib> // Include for random number generation
 
+void Sputnik::breakApart(std::vector<OrbitObject*>& orbitObjects) const {
    Velocity vel(velocity);
    vel.addMetersX(1000);
 
-   Fragment* fragments[3];
-   int spacing = 10;
-   for (int i = 0; i <= 2; i++) {
+   const int fragmentCount = 5; // Number of fragments
+   Fragment* fragments[fragmentCount];
+   const int spacing = 100; // Spacing in degrees (for a circular pattern)
+
+   for (int i = 0; i < fragmentCount; i++) {
       fragments[i] = new Fragment();
       Position pos(position);
-      pos.addPixelsX(i += spacing);
-      pos.addPixelsY(i -= spacing);
-      fragments[i]->setRotation(rotation.getAngle());
+
+      // Calculate the position for each fragment in a circular pattern
+      float angle = (spacing * i) * (M_PI / 180.0); // Convert degrees to radians
+      float xOffset = cos(angle) * 10; // 10 is the radius of the circle
+      float yOffset = sin(angle) * 10;
+
+      pos.addPixelsX(xOffset);
+      pos.addPixelsY(yOffset);
+
+      // Generate a random rotation for each fragment
+      float randomRotation = static_cast<float>(rand() % 360); // Random angle between 0 and 359 degrees
+
+      fragments[i]->setRotation(randomRotation);
       fragments[i]->setPosition(pos);
       fragments[i]->setVelocity(vel);
       orbitObjects.push_back(fragments[i]);
    }
-
 }
