0d3e5d0f96fb49af79181fbbc52dda39febca457
Brodie M Busby <93949221+brodiembusby@users.noreply.github.com>
2023-12-02 09:22:32 -0700

    comment out bad code

--- part.h
+++ part.h
@@ -6,7 +6,7 @@ class Part : public OrbitObject
 {
 public:
   
-   ogstream DrawFunctionType = std::function<void( Position, Rotation)>;
+   //ogstream DrawFunctionType = std::function<void( Position, Rotation)>;
 
    void draw(ogstream& gout) const override { gout.drawCrewDragonCenter(position, rotation.getAngle()); };
    void breakApart(std::vector<OrbitObject*>& orbitObjects) const override;
