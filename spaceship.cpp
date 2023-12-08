daa4b5a84c26cc8c4e40b1e4a8bf192c7359fb31
AdvancedEel850 <90942089+AdvancedEel850@users.noreply.github.com>
2023-12-07 21:41:53 -0700

    fixed redundancy issue

--- spaceship.cpp
+++ spaceship.cpp
@@ -72,18 +72,3 @@ void Spaceship::breakApart(std::vector<OrbitObject*>& orbitObjects) const {
       orbitObjects.push_back(createFragment(angle*i));
    }
 }
-
-void Spaceship::update(const double time)
-{
-   // Set acceleration to gravity
-   Acceleration acceleration(position.getGravity());
-   updateCallback(acceleration);
-
-   // Update velocity
-   const Velocity& dV = acceleration.getDeltaVelocity(time);
-   velocity += dV;
-
-   // Update position
-   const Position dP = velocity.getDeltaPosition(time);
-   position += dP;
-}
